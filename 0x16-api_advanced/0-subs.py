44bdaba661154de832cc669e2b62a11edbe2dc88
OjModupe <134464057+OjModupe@users.noreply.github.com>
2024-06-06 12:05:12 +0000

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-""" This module queries the Reddit API and returns
-the number of subscribers (not active users, total subscribers)
-for a given subreddit. If an invalid subreddit is given,
-the function should return 0
-"""
+""" This module contains a function that queries
+    RedditAPI for subreddits """
 
 import json
 import requests
@@ -11,14 +8,19 @@ import sys
 
 
 def number_of_subscribers(subreddit):
-    """ This function returns the number of subscribers
-    for a given subreddit. If an invalid subreddit is given,
-    the function should return 0
-    """
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'My User Agent 1.0'}
-    data = requests.get(url, headers=headers)
-    if data.status_code >= 300:
-        return 0
+    """ Returns the number of subscribers for a given subreddit """
+    user_agent = 'Mozilla/5.0'
+    headers = {
+        'User-Agent': user_agent
+    }
 
-    return (data.json().get('data').get('subscribers'))
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return 0
+    subs = response.json()
+    if 'data' not in subs:
+        return 0
+    if 'subscribers' not in subs.get('data'):
+        return 0
+    return response.json()['data']['subscribers']
